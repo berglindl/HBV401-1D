b7545b054fdbf88bb7549f30b3039a9d4f66eda7
Vitringur <pgi1@hi.is>
2016-03-23 18:07:27 +0000

    Minniháttar viðbætur
    Setti smáræði inn í Tripinventory og trip

--- Trip.java
+++ Trip.java
@@ -1,4 +1,17 @@
 public class Trip{
+	
+	private String name;
+	private String type;
+	
+	public void setName(String n){
+		name = n;
+	}
+	
+	public void setType(String t){
+		type = t;
+	}
+	
+	
 	public void compare(){
 		
 	}
--- TripInventory.java
+++ TripInventory.java
@@ -1,5 +1,31 @@
 public class TripInventory extends Trip{
+	
+	
+	//Vænanlega geymum við ferðirnar inn í fylki
+	private Object[] Inventory;
+	
+	//gæti verið heppilegt til að vita hve margar ferðir eru í 
+	//fylkinu og í hvaða sæti næsta ferð fer.
+	int count;
+	
+	
+	//Til bráðabirgða allavega þá hugsa ég sniðugt að hafa
+	//svona fall til að búa til Trip hlutina  
+	//spurning hvernig þetta tengist við sql seinna
+	public void maketrip(String name, String type, Object[] information){
+		Trip a = new Trip;
+		a.setname(name);
+		a.settype(type);
+		a.maketripproperties(information);
+		Inventory[count++]=a;
+	}
+	
+	
+	
 	public void search(){
 		
 	}
+	
+	
+	
 }
