7e43e19897330169085fdf3f3b1db14c5fd2b73e
berglindl <berglindpuma@gmail.com>
2016-03-30 20:05:08 +0000

    Viðbót í tripProperties
    Er búin að setja inn dálítið hér, á bara eftir að setja inn tripLength,
    additionalInfo, tripDate og tripPrice sem ég hendi öruglega inn á
    morgun. Hvernig lýst ykkur á að hafa þetta svona?

--- TripProperties.java
+++ TripProperties.java
@@ -10,6 +10,46 @@ public class TripProperties extends Trip{
 		return 2;
 	}
 
+	//cardDir decides which of those cardinal directions are chosen
+	//cardDir = 1 means North, cardDir = 2 means East, ...
+	public String location(String cardDirString){
+		int cardDir;
+		switch(cardDir){
+			case 1: cardDirString = "Norður";
+					break;
+			case 2: cardDirString = "Austur";
+					break;
+			case 3: cardDirString = "Suður";
+					break;
+			case 4: cardDirString = "Vestur";
+					break;
+		}
+		return cardDirString;
+	}
+
+	//Mögulega hafa þetta sett upp svona líka?
+	public String language(String selectLanguageString){
+		int selectLanguage;
+		switch(selectLanguage){
+			case 1: selectLanguageString = "ISL";
+					break;
+			case 2: selectLanguageString = "ENG";
+					break;
+			case 3: selectLanguageString = "CHI";
+					break;
+			//má bæta við fleiri af vild
+		}
+		return selectLanguageString;
+	}
+
+	//Mock object því þetta tengist booking, right?
+	public int numSeats(int seat){
+		int min = 1;
+		int max = 50;
+		//returns a random number between 1 and 50
+		return (Math.random() * (max - min) + min)
+	}
+
 	//setja override? 
 	public void getInfo(){
 
