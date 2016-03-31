a06aa394d768476de7a5e71ebdc3abb3be49e478
berglindl <berglindpuma@gmail.com>
2016-03-31 18:22:04 +0000

    Meiri viðbót í tripProperties
    Bætti við restinni í tripProperties, sumt er mock object, annað er
    útfært á einfaldan hátt.

--- TripProperties.java
+++ TripProperties.java
@@ -43,11 +43,79 @@ public class TripProperties extends Trip{
 	}
 
 	//Mock object því þetta tengist booking, right?
+	//For now returns a random number between 1 and 50
 	public int numSeats(int seat){
-		int min = 1;
+		int min = 0;
 		int max = 50;
-		//returns a random number between 1 and 50
-		return (Math.random() * (max - min) + min)
+		seat = Math.random() * (max - min) + min
+		if(seat == 0){
+			System.out.println("Booking is full! Try another date.");
+		}
+		else{
+			return seat;
+		}
+	}
+
+	//The length of the trip - length determines what case is used
+	//while the value of num determines the outcome
+	public int tripLength(int length){
+		int num = 0;
+		switch(length){
+			case 1: num = 4;
+					break;
+			case 2: num = 7;
+					break;
+			case 3: num = 10;
+					break;
+		}
+		return num;
+	}
+
+	//Gæti verið að við þurfum að breyta þessum klasa því hann er frekar ljótur 
+	//en hann á allaveganna að virka svona þannig að ... 
+	public String additionalInfo(String addInfoString){
+		int addInfo;
+		switch(addInfo){
+			//Trip Into the Glacier
+			case 1: addInfoString = "We start by hiking on Falljökull glacier tongue and from there we explore the cave and a nearby ice tunnel. The tour is highly dependent on the cave's condition, so it must be treated with the utmost care and respect. " + "Clothing suggestion: Warm and waterproof jacket and pants, hiking boots, headwear and gloves.";
+					break;
+			//Trip Skíðaferð á Hrútafjallstinda
+			case 2: addInfoString = "We start at Hafrefell nearby Svínafellsjökull and head up to the top of Hrútafjallstind where we put on our skiis and head down. This is a great sport that has been practiced since the country was settled. " + "Clothing suggestions: Warm and waterproof clothing.";
+					break;
+			//Trip Whale Watching in Húsavík
+			case 3: addInfoString = "Join us on a day trip and meet the friendly giants of the sea in the breathtaking surroundings of Skálfjandflói Bay. Included is a three hour boat tour. " + "Clothing suggestions: Warm and waterproof clothing.";
+					break;
+			//Trip Söguferð um Miðbæinn
+			case 4: addInfoString = "Join us in a walk through Reykjavík downtown where the participants will be informed about the long history of the city and other exciting information. If you seek knowledge and love to travel, this is the trip for you! " + "Clothing suggestions: Warm clothing and good walking shoes.";
+					break;
+			//Trip Northern Lights Explorer	
+			case 5: addInfoString = "A super jeep tour where we leave the lights of the city behind and head into the darkness for an adventure. On the search for the Northern Lights, we might travel through some of Iceland's most spectacular sights without knowing, but the guide will be sure to let you know! " + "Clothing suggestions: Warm outwear.";
+					break;
+		}
+		return addInfoString;
+	}
+
+	//Mock object ?? 
+	//For now only shows three dates, first, second and third of may. 
+	public String tripDate(String dateString){
+		int date;
+		switch(date){
+			case 1: dateString = "01.05";
+					break;
+			case 2: dateString = "02.05";
+					break;
+			case 3: dateString = "03.05";
+					break;
+		}
+		return dateString;
+	}
+
+	//Potential mock object? Just returns a random number between min and max
+	public int tripPrice(int price){
+		int min = 5000;
+		int max = 25000;
+		//returns a random number between 5000 and 25000
+		return (Math.random() * (max - min) + min);
 	}
 
 	//setja override? 
