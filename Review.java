cc8e8594c07c54e811c7f4b58e47fc7140de6a04
hildurhalla <hildur_711@hotmail.com>
2016-04-09 14:30:24 +0000

    Byrjjun, þarf að laga vel
    Byrjaði aðeins á kóðanum hér, það á eftir að bæta við tengingum við
    allar ferðir og þannig þetta vistist í lista eins og í booking. Var ekki
    viss hvernig við vildum hafa þetta í lokin og kóðinn er mjög skringilega
    upp settur þannig endilega breytið :)

--- Review.java
+++ Review.java
@@ -1,3 +1,46 @@
-public class Review{
-	//...
+import java.util.Scanner;
+class review
+// var ekki alveg viss hvernig ég ætti að gera þetta þannig ákvað að henda up smá byrjun
+// Þetta var fyrsta útgáfan þar sem ég fékk ekki villu í keyrslu þannig hélt henni inni en 
+// eins og þið sjáið þá er þetta ekki fallegt
+{
+    // Hér þyrfti að vera einhver tenging við ferðirnar en var ekki viss hvernig við vildum gera þetta
+    // þannig tengdi ekki við tripNames eða neitt svoleiðis ennþá
+ public static void main(String [] args)
+    {
+     int vote;
+     int votesCount = 0;
+     // Núlstillti bara til að byrja með þar sem það er enging tengin við neitt komin
+     // breytum þessu á endanum
+     Scanner userInput = new Scanner(System.in);
+     {
+        System.out.println("Rate this trip on a scale from 0-5");
+        vote = userInput.nextInt();
+        votesCount++;
+        // Setti þetta inn hér því annars var hann ekki að telja 
+        // Endilega lagið þetta ef þið getið
+        if (vote < 0)
+        {
+         vote = 0;
+        }
+        if (vote > 5)
+        {
+         vote = 5;
+        }
+        // Megið fegra þetta ef þið getið
+     }
+  int votesTotal;
+    {
+       votesTotal = vote;
+       votesTotal = votesTotal + vote;
+     }
+   if (votesCount > 0)
+    // Tilgangslaust að telja ef það eru engin atkvæði
+    {
+        double average = votesTotal / votesCount;
+        System.out.println("Average for this trip is:" + average );
+    }
+   userInput.close();
+   // átti eftir að bæta inn svo þetta færi allt í sama lista
+ }
 }
