dff05b567afa0c2b66c8449c57e67678dfede546
berglindl <berglindpuma@gmail.com>
2016-04-11 19:38:13 +0000

    Database
    Bjó til database via sqlite3. Þetta er db file þannig að það þarf að
    vista það þar sem þið höfðuð hin db file-in síðan úr gagnasafnsfræði.
    Setti inn txt skjal sem er með frekari upplýsingar up database-inn.

--- TripProperties.java
+++ TripProperties.java
@@ -93,15 +93,17 @@ public class TripProperties extends Trip{
 					break;
 			case 2: num = 4 + " hours";
 					break;
-			case 3: num = 7 + " hours";
+			case 3: num = 5 + " hours";
 					break;
-			case 4: num = 8 + " hours";
+			case 4: num = 7 + " hours";
 					break;
-			case 5: num = 9 + " hours";
+			case 5: num = 8 + " hours";
 					break;
-			case 6: num = 10 + " hours";
+			case 6: num = 9 + " hours";
 					break;
-			case 7: num = 12 + " hours";
+			case 7: num = 10 + " hours";
+					break;
+			case 8: num = 12 + " hours";
 					break;
 		}
 		return num;
@@ -119,70 +121,78 @@ public class TripProperties extends Trip{
 			//Trip Into the Glacier
 			case 1: addInfoString = "We start by hiking on Falljökull glacier tongue and from there we explore the cave and a nearby ice tunnel. 
 									The tour is highly dependent on the cave's condition, so it must be treated with the utmost care and respect. " 
-									+ "Clothing suggestion: Warm and waterproof jacket and pants, hiking boots, headwear and gloves.";
+									+ "Clothing suggestion: Warm and waterproof outwear.";
 					break;
 			//Trip Skíðaferð á Hrútafjallstinda
-			case 2: addInfoString = "We start at Hafrafell nearby Svínafellsjökull and head up to the top of Hrútafjallstind where we put on our skiis and head down. 
+			//Skiing on Hrútafjallstindur
+			case 2: addInfoString = "We start at Hafrafell nearby Svínafellsjökull and head up to the top of Hrútafjallstindur where we put on our skiis and head down. 
 									This is a great sport that has been practiced since the country was settled. " 
-									+ "Clothing suggestions: Warm and waterproof clothing.";
+									+ "Clothing suggestion: Warm and waterproof outwear.";
 					break;
 			//Trip Whale Watching in Húsavík
 			case 3: addInfoString = "Join us on a day trip and meet the friendly giants of the sea in the breathtaking surroundings of Skálfjandflói Bay. 
-									Included is a three hour boat tour. " + "Clothing suggestions: Warm and waterproof clothing.";
+									Included is a three hour boat tour. " + "Clothing suggestion: Warm and waterproof outwear.";
 					break;
 			//Trip Söguferð um Miðbæinn
-			case 4: addInfoString = "Join us in a walk through Reykjavík downtown where the participants will be informed about the long history of the city 
+			//History Walk Through Downtown
+			case 4: addInfoString = "Join us in a walk through Reykjavík downtown where the participants will be informed about the rich history of the city 
 									and other exciting information. If you seek knowledge and love to travel, this is the trip for you! " 
-									+ "Clothing suggestions: Warm clothing and good walking shoes.";
-					break;
-			//Trip Northern Lights Explorer	
-			case 5: addInfoString = "A super jeep tour where we leave the lights of the city behind and head into the darkness for an adventure. 
-									On the search for the Northern Lights, we might travel through some of Iceland's most spectacular sights without knowing, 
-									but the guide will be sure to let you know! " + "Clothing suggestions: Warm outwear.";
+									+ "Clothing suggestion: Warm clothing and good walking shoes.";
 					break;
 			//Trip the Golden Circle and Snorkeling
-			case 6: addInfoString = "First stop is at Þingvellir National Park, where diving will take place in the Silfra Fissure. 
+			case 5: addInfoString = "First stop is at Þingvellir National Park, where diving will take place in the Silfra Fissure. 
 									From there, we head to the Geysir hot spring, and then finally to the golden waterfall, Gullfoss. "
-									+ "Clothing suggestions: Warm and waterproof outdoor clothing, and a change of clothing. Good hiking shoes are recommended. ";
+									+ "Clothing suggestion: Warm outdoor clothing, a change of clothing, and good hiking shoes.";
 					break;
 			//Trip Glacier Walking and Waterfalls
-		    case 7: addInfoString = "An exciting tour along the south coast visiting all the major sites of the area and hiking on a glacier. 
+		    case 6: addInfoString = "An exciting tour along the south coast visiting all the major sites of the area and hiking on a glacier. 
 		    						First stop is at Seljalandfoss waterfall, then to Sólheimajökull glacier, to Reynisfjara's black sand beach, to Skógafoss waterfall, 
-		    						and with a view to Eyjafjallajökull Glacier/Volcano, and the Hekla volcano. "
-		    						+ "Clothing suggestions: Warm outdoor clothing and good hiking shoes.";
+		    						and with a view to Eyjafjallajökull and Hekla volcano. "
+		    						+ "Clothing suggestion: Warm outdoor clothing and good hiking shoes.";
 		    		break;
+		    //Trip Northern Lights Explorer	
+			case 7: addInfoString = "A super jeep tour where we leave the lights of the city behind and head into the darkness for an adventure. 
+									On the search for the Northern Lights, we might travel through some of Iceland's most spectacular sights without knowing, 
+									but the guide will be sure to let you know! " + "Clothing suggestion: Warm outwear.";
+					break;
 		    //Trip Game of Thrones - Mývatn Mystery Magic
 		    case 8: addInfoString = "Join us on a tour to magical Lake Mývatn and the filming sites of the latest Game of Thrones series, with a pitstop to the Mývatn Nature Baths. "
-		    						+ "Clothing suggestions: Warm outdoor clothing.";
+		    						+ "Clothing suggestion: Warm outdoor clothing.";
 		    		break;
 		    //Trip Papeyjarferðir
-		    case 9: addInfoString = "A day trip from Djúpivogur to Papey. We sail around the area and watch the birds and the seals in the area. 
+		    //A Trip to Papey Islands
+		    case 9: addInfoString = "A day trip from Djúpivogur to Papey. We sail around the area and watch the birds and the seals that reside nearby.
 		    						After that, we stop by on the island where we walk along with a guide. "
-		    						+ "Clothing suggestions: Warm and waterproof clothing.";
+		    						+ "Clothing suggestion: Warm and waterproof outwear.";
 		    		break;
 		    //Trip Hellaferð við Ingólfsfjall
+		    //The Caves in Ingólfsfjall
 		    case 10: addInfoString = "Join us on a search for the ancient treasure in the caves of the mountain Ingólfsfjall. This trip is perfect for adventurers! "
-		    						+ "Clothing suggestions: Warm outdoor clothing";
-		    		break;
-		    //Trip Spennuganga um Viðey
-		    case 11: addInfoString = "Meet us on the island Viðey where we go on an exciting walk through the island where we try to solve the mystery of the lost elves
-		    						and the hidden people who are said to be the islands former residents. "
-		    						+ "Clothing suggestions: Warm and waterproof outdoor clothing. ";
+		    						+ "Clothing suggestion: Warm outdoor clothing and good hiking shoes.";
 		    		break;
 		    //Trip Á Gönguskíðum við Langanes
-		    case 12: addInfoString = "Join us on a skiing trip at Langanes where you will be educated about the secrets of Langanes and the exciting facts about its capital, Þórshöfn.
+		    //Skiing through Langanes
+		    case 11: addInfoString = "Join us on a skiing trip at Langanes where you will be educated about the secrets of Langanes and the exciting facts about its capital, Þórshöfn.
 		    						The participants must have all equipment beforehand, but will be picked up by super jeeps and driven to Langanes. "
-		    						+ "Clothing suggestions: Warm and waterproof outdoor clothing. ";
+		    						+ "Clothing suggestion: Warm and waterproof outdoor clothing. ";
 		    		break;
 		    //Trip Grásleppur, Hvalir og Kynjaverur
-		    case 13: addInfoString = "The boat Nói will take up to Breiðafjörður where we will look at various dwellers of the sea. If we're lucky, participants might have the chance
+		    //Whale Watching in Breiðafjörður
+		    case 12: addInfoString = "The boat Nói will take up to Breiðafjörður where we will look at various dwellers of the sea. If we're lucky, participants might have the chance
 		    						to feast upon whale meat from a freshly hunted whale. "
-		    						+ "Clothing suggestions: Warm and waterproof outdoor clothing. ";
+		    						+ "Clothing suggestion: Warm and waterproof outwear. ";
 		    		break;
 		    //Trip Vestfirsk dansferð
-		    case 14: addInfoString = "A bus will drive the participants to Ísafjörður where the participants have the option to dance with one another. 
+		    //Western Dancing Trip
+		    case 13: addInfoString = "A bus will drive the participants to Ísafjörður where the participants have the option to dance with one another. 
 		    						This trip is perfect for those who wish to meet new people and to show off their dancing skills! "
-		    						+ "Clothing suggestions: Comfortable clothing that allow you to bust a move!";
+		    						+ "Clothing suggestion: Comfortable clothing that will allow you to bust a move!";
+		    		break;
+		    //Trip Spennuganga um Viðey
+		    //A Walk Through Viðey
+		    case 14: addInfoString = "Meet us on the island Viðey where we go on an exciting walk through the island where we try to solve the mystery of the lost elves
+		    						and the hidden people who are said to be the islands former residents. "
+		    						+ "Clothing suggestion: Warm and waterproof outwear. ";
 		    		break;
 		}
 		return addInfoString;
