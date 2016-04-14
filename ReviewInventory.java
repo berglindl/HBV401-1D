3bffee0f38a5bcaad6b7953d341704c4104c6d18
hildurhalla <hildur_711@hotmail.com>
2016-04-14 15:04:27 +0000

    Lagaði review
    review kóðinn virkar en var ekk viss hvernig við vildum vista hann,
    setti inn athugasemd um það í kóðann þannig kíkið kannsk bara á það við
    tækifæri

--- ReviewInventory.java
+++ ReviewInventory.java
@@ -6,19 +6,21 @@ class Update
  //Hér þyrfti tengingu við ferðirnar
  public static void main(String[] args) 
  {
+   String reviewName;
    String reviewText;
+   String reviews;
    //megið breyta nafninu, datt bara ekkert betra í hug í bili
    Scanner userInput = new Scanner(System.in);
    System.out.println("Type in your review");
-   reviewText = userInput.next();
-   System.out.println(reviewText);
-   ReviewList.add(reviewText);
-   // Gætum þá séð lista af reviews, á að bæta við username í listann eða viljum við hafa  
-   // þetta naflaust? 
-
-   //Ég held að user name gæti verið fínt, en er ReviewList fylki eða eitthvað þannig?
+   reviewText = userInput.nextLine();
+   System.out.println("Type in your name");
+   reviewName = userInput.next();
+   reviews = reviewName + " wrote: " + reviewText;
+   System.out.println(reviews);
+   // Spurning hvernig við viljum vista reviews. Eins og er er bara verið að prenta allt en spurning hvort þið vljið gera það svona, eða e-ð öðruvísi
+   // ReviewList.add(reviews); 
+   // Fer eftir því hvernig við viljum geyma þetta, en kóðinn vrkar eins og er þannig hann er komin
+    
    userInput.close();
    }
 }
-
-//Tók út delete því ég held að það sé algjör óþarfi eins og verkefnið stendur núna
